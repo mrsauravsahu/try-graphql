8d0eb302be8986b9e7d3f375cf45d96ad63eb3dd
Saurav Sahu <mrsauravsahu@outlook.com>
2020-11-13 19:01:58 +0530

    feat: add a graphQL endpoint (#1)
    * install packages for graphql
    
    * import regenerator-runtime in server for type-graphql
    
    * create types for graphQL schema
    
    * add meta resolver
    
    * add graphQL endpoint
    
    * attach graphQL endpoint
    
    * todo: validation

---
+++ app/Controllers/Http/GraphQLRootController.ts
@@ -0,0 +1,18 @@
+import { MetaResolver } from 'App/GraphQL/Resolvers/MetaInfoResolver'
+import { buildSchema } from 'type-graphql'
+import { graphql } from 'graphql'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class GraphQLRootController {
+  public async index({ request }: HttpContextContract) {
+    // TODO: Some sort of validation?
+    var graphQLQuery = request.input('query')
+
+    var schema = await buildSchema({
+      resolvers: [MetaResolver],
+    })
+
+    var response = await graphql(schema, graphQLQuery)
+    return response
+  }
+}
