747456d8752eeadac8140787efa5077b14ee2748
Saurav Sahu <mrsauravsahu@outlook.com>
2020-11-14 15:55:57 +0530

    add addTodo mutation to create a todo

--- database/migrations/1605339223649_todos.ts
+++ database/migrations/1605339223649_todos.ts
@@ -1,11 +1,10 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
-
 export default class Todos extends BaseSchema {
   protected tableName = 'todos'
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.uuid('id')
+      table.uuid('id').primary()
       table.timestamps(true)
       table.string('title')
       table.string('description')
